35e44f78ed8cd650cb2ac1bf91027765f2096b0f
Marc Schweigert <marcsc@microsoft.com>
2022-01-31 09:16:12 -0500

    long path workaround

--- pack.ps1
+++ pack.ps1
@@ -3,13 +3,24 @@ function Pack ($solutionName) {
   $vsSolutionPath = "./src/SelfContainedSolutionTest/code-first"
   $unpackFolder = "src/$solutionName/unpacked-solution"
   $solutionZip = "$packTemp/$solutionName.zip"
+  $unPackTemp = "$packTemp/u"
+
+  if (Test-Path $unPackTemp) {
+    Remove-Item "$unPackTemp/*" -Recurse -Force
+  }
+  else {
+    New-Item -Path $unPackTemp -ItemType Directory
+  }
+
+  
+  Copy-Item "$unpackFolder/*" $unPackTemp -Recurse -Force
 
   $source = "$vsSolutionPath/Plugins/bin/Release/SelfContainedSolutionTest.Plugins.dll"
   $destination = "$unpackFolder/PluginAssemblies/SelfContainedSolutionTestPlugins-0D938548-626C-4DC8-8AAB-08E6DAD8BA92/SelfContainedSolutionTestPlugins.dll"
   Copy-Item $source $destination -Force
 
-  #pac solution pack --zipfile $solutionZip --folder $unpackFolder --packagetype both
-  pac solution pack --zipfile $solutionZip --folder $unpackFolder --packagetype unmanaged
-  pac solution pack --zipfile $solutionZip.Replace(".zip","_managed.zip") --folder $unpackFolder --packagetype managed
+  #pac solution pack --zipfile $solutionZip --folder $unPackTemp --packagetype both
+  pac solution pack --zipfile $solutionZip --folder $unPackTemp --packagetype unmanaged
+  pac solution pack --zipfile $solutionZip.Replace(".zip","_managed.zip") --folder $unPackTemp --packagetype managed
 }
 Pack "SelfContainedSolutionTest"
